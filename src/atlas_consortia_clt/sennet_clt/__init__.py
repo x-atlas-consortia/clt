03fd0070a4034b8e2580acb9d8e123cea837cbc8
Tyler Madonna <tjm159@pitt.edu>
2023-07-07 15:08:04 -0400

    Updated sennet docs url

--- src/atlas_consortia_clt/sennet_clt/__init__.py
+++ src/atlas_consortia_clt/sennet_clt/__init__.py
@@ -8,6 +8,6 @@ def entry():
         ingest_url = appcfg["SENNET_INGEST_URL"],
         entity_id = "SNT123.ABCD.456",
         entity_id_name = "sennet_id",
-        docs_url = "https://docs.sennetconsortium.org/clt",
+        docs_url = "https://docs.sennetconsortium.org/libraries/clt",
     )
     main.launch_command(cfg)
